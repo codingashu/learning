79f751195ec5b0de6aa950f3ab49b0c36990f300
codingashu <80610752+codingashu@users.noreply.github.com>
2024-08-27 06:26:34 +0000

    new file: datatypes.java

---
+++ datatypes.java
@@ -0,0 +1,20 @@
+public class datatypes {
+    public static void main(String[] args){
+        boolean a = true;
+        System.out.println(a);
+        byte b = 8;
+        System.out.println(b);
+        char c = 65;
+        System.out.println(c);
+        int d = 65;
+        System.out.println(d);
+        short e = 150;
+        System.out.println(e);
+        long f = 15000000l;
+        System.out.println(f);
+        float g = 100.98f;
+        System.out.println(g);
+        double h = 101.239234d;
+        System.out.println(h);
+    }
+}
