0dabcdda3cc4477297151736adf65d7c25b70160
codingashu <80610752+codingashu@users.noreply.github.com>
2024-08-28 03:33:28 +0000

    variables

---
+++ variables.java
@@ -0,0 +1,17 @@
+public class variables {
+    public static void main(String[] args){
+    int var1 = 25;
+    if(var1>20){
+        int var2 = 10 + var1;
+        System.out.println(var2);
+    }
+    variables  obj = new variables();
+    obj.display();
+}
+    static int b = 10;
+    public int a = 15;
+    public void display(){
+        System.out.println(a);
+    }
+}
+
