91973a4405f9f07947f5b062f13d3e7e84f9366a
codingashu <80610752+codingashu@users.noreply.github.com>
2024-08-30 12:06:35 +0000

    converstring

---
+++ convertstring.java
@@ -0,0 +1,15 @@
+public class convertstring{
+	public static String convertString(String str) {
+          char[] c = str.toCharArray();
+          c[0]=Character.toUpperCase(c[0]);
+          for(int i=0;i<c.length-1;i++){
+              if(c[i]==' '){
+                  c[i+1]=Character.toUpperCase(c[i+1]);
+              }
+              // Write your code here
+        }
+        return new String(c);
+
+}
+}
+
