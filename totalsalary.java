bea0d681e63b272ed67a52a14c8c4247ffe6d07a
codingashu <80610752+codingashu@users.noreply.github.com>
2024-08-31 09:17:00 +0000

    totalsalary

--- totalsalary.java
+++ totalsalary.java
@@ -14,3 +14,23 @@ public class totalsalary {
 		//Write your code here
 	}
 }
+/*Ninja just got an offer letter from a reputable company. The company sent him an offer letter along with the salary bifurcation.
+
+In that bifurcation,Total Salary was not mentioned but instead a ‘basicSalary’ and an upper case character representing grade was mentioned, depending on which the Total Salary is calculated.
+
+Help Ninja in calculating his total salary, where total salary is defined as:
+
+‘totalSalary’ = ‘basic’ + ‘hra’ + ‘da’ + ‘allowance’ - ‘pf’
+The above terms are as follows:
+
+‘hra’ = 20% of ‘basic’
+‘da’ = 50% of ‘basic’
+‘allowance’ = 1700 if grade = ‘A’
+‘allowance’ = 1500 if grade = ‘B’
+‘allowance’ = 1300 if grade = ‘C' or any other character
+‘pf’ = 11% of ‘basic’.
+Note :
+
+Round off the ‘totalSalary’ and then print the integral part only.
+
+'x.5' type values will always be round up, for example, 1.5, 2.5 will be round off to 2, 3 respectively. */
