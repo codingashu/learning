4b436caf2a33b89f466c14f123911a93e9b08da3
codingashu <80610752+codingashu@users.noreply.github.com>
2024-08-31 04:50:27 +0000

    selectionsort

---
+++ selectionsort.java
@@ -0,0 +1,16 @@
+public class selectionsort {
+    public static void selectionSort(int arr[], int n) {
+		for(int i=0;i<=n-2;i++){
+		int min = i;
+		for(int j=i;j<=n-1;j++){
+			if(arr[j]<arr[min]){
+				min=j;
+			}
+		}
+		int temp = arr[min];
+		arr[min]=arr[i];
+		arr[i]=temp;
+			// Write your code here.
+	}
+}
+}
